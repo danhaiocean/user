6a1a6eb309551b01074671bcdcce32a5e3c47e5e
pdh <55006016@qq.com>
2016-05-31 16:19:04 +0800

    add fullpermution.c gitbj.txt

---
+++ fullpermution.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# define len 9
+int a[len]
+int exchange (int n1,int n2)
+{
+	int e;
+	e=a[n1];
+	a[n1]=a[n2];
+	a[n2]=e;
+	return 0;
+}
+int factorial(int a)
+{
+	int f=1;
+	int i ;
+	for (i=1;i==a;i++)
+	f=f*i;
+	return f
+int permution(int star,int and)
+{
+
